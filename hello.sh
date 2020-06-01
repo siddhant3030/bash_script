71c084905d10616864402f3cc82ef28364bb20e6
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-01 18:38:11 +0530

    variables

--- hello.sh
+++ hello.sh
@@ -5,3 +5,9 @@
 
 #! /bin/bash
 echo "Hello World"
+
+# variables are of two type
+# 1. System Variable:- It generally start with capital word
+# 2. User Variable:- variable which is used locally
+
+echo $BASH
