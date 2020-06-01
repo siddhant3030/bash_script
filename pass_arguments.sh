a1804657b0688b7dcd5338b22c501fc47d6cfb57
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-02 03:00:56 +0530

    adding more commands

--- pass_arguments.sh
+++ pass_arguments.sh
@@ -15,3 +15,18 @@ echo $0 $1 $2 $3 '> echo $1 $2 $3'
 # ./pass_arguments.sh to sd ad > echo $1 $2 $3
 
 # As you can see here we're getting the 0th index with ./pass_arguments.sh
+
+# 3.
+
+# We can pass the result into array. Let's see that
+
+args = ("$@")
+
+# This $@ stores your argument as an array. So to pass them you need to declare
+# a variable then you assign those argument which are stored in $@ 
+
+echo ${args[0]} ${args[1]} ${args[2]}
+
+# We can return all the value by just this also. Above echo is for reading arguments
+# by there index
+echo $@
