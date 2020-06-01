17f2293791baffe4615e2c0e5bd2d03ab2e85238
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-01 20:27:18 +0530

    adding comments

--- input.sh
+++ input.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Read user input from the command
-# echo "enter name: "
-# read name
-# echo "entered name: $name"
+echo "enter name: "
+read name
+echo "entered name: $name"
 
 # Result
 # enter name: 
@@ -11,10 +11,14 @@
 # entered name: siddhant
 
 # Reading Multiple Input
-# echo "enter names: "
-# read name1 name2 name3
-# echo "Names: $name1, $name2, $name3"
+echo "enter names: "
+read name1 name2 name3
+echo "Names: $name1, $name2, $name3"
 
 # Print the username on the same line
-read -p 'username: ' user_var
+read -p 'username: ' user_var # -p is a flag here 
 echo "username: $user_var"
+
+# Example
+# username: sid
+# username: sid
