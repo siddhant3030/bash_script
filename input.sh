d3eb40b5ab5454050d954958dde6316368fe6567
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-01 18:55:14 +0530

    reading the username on same line

--- input.sh
+++ input.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 
 # Read user input from the command
-echo "enter name: "
-read name
-echo "entered name: $name"
+# echo "enter name: "
+# read name
+# echo "entered name: $name"
 
 # Result
 # enter name: 
 # siddhant
 # entered name: siddhant
+
+# Reading Multiple Input
+# echo "enter names: "
+# read name1 name2 name3
+# echo "Names: $name1, $name2, $name3"
+
+# Print the username on the same line
+read -p 'username: ' user_var
+echo "username: $user_var"
