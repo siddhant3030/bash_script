10bd1e561b2f71710d5847f0fbc6c0965b866eb3
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-02 02:44:01 +0530

    updated the readme

--- array.sh
+++ array.sh
@@ -4,3 +4,12 @@
 echo "Enter names: "
 read -a names
 echo "Names: ${names[0]}, ${names[1]}"
+
+
+
+# If we don't put any variable in front of read commands then it goes to 
+# inbuilt variable $REPLY
+
+echo "Enter names: "
+read 
+echo "Names: $REPLY"
