17497706269a15864d9d09d1f32af8f6acb8df24
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-02 18:40:47 +0530

    comments added

--- file.sh
+++ file.sh
@@ -10,3 +10,9 @@ then
 else
     echo "$file_name not found"
 fi
+
+# Shell Script has many operators that you can use to test different aspects of a file.
+#  For example, you can use the -e operator to ensure that a file exists before deleting it.
+#   Or, you can check that a file can be written to before appending to it.
+#    By checking the feasibility of the impending file operation,
+#     you can reduce the number of errors that your program will encounter.
