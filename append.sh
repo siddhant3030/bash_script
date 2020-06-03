b32c1ab2c1d1574e951b6fbb2cf46b6d5c32f4d0
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-04 00:00:54 +0530

    adding more fun

--- append.sh
+++ append.sh
@@ -3,9 +3,19 @@
 echo -e "Enter the name of the file : \c"
 read file_name
 
+# >> means to append
+# > means to overwite the file
+
+
 if [ -f $file_name ]
 then
-    echo "$file_name found"
+    if [ -w $file_name ]
+    then
+        echo "Type some text. To quit press ctrl+d"
+        cat >>
+    else
+
+    fi
 else
     echo "$file_name not found"
 fi
