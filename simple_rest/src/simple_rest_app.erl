b16089af67dd655430c5614ebb78e28432584f38
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-21 22:59:10 +0530

    supervisor

--- simple_rest/src/simple_rest_app.erl
+++ simple_rest/src/simple_rest_app.erl
@@ -5,7 +5,16 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	simple_rest_sup:start_link().
+    Dispatch = cowboy_router:compile([
+        {'_', [
+               {"/", rest_time_handler, []}
+              ]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener, 100,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    simple_rest_sup:start_link().
 
 stop(_State) ->
 	ok.
